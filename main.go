e7dd3ca63bdc73e80b8d398873103f08d485c587
Oliver Looney <oliverjlooney@gmail.com>
2022-06-30 19:35:25 +0100

    create user's folder and init git

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"os/exec"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	err := os.Mkdir("/home/oliver/MyGitHub/"+os.Args[1], 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// exec.Command("cd", "/home/oliver/MyGitHub/projectSetUpToolGolang/"+os.Args[1])
+	// err2 := exec.Command("touch", "README.md")
+	// if err2 != nil {
+	// 	log.Fatal(err)
+	// }
+	os.Chdir("/home/oliver/MyGitHub/" + os.Args[1])
+	// cmd := exec.Command("cd", "/home/oliver/MyGitHub/projectSetUpToolGolang/"+os.Args[1])
+	// if err := cmd.Run(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	exec.Command("touch", "README.md").Run()
+	exec.Command("git", "init").Run()
 }
